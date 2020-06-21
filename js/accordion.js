62573135b83ed7a2204edab92ee4626fb3fb1757
Elempiar <Lverboeket@hotmail.com>
2020-06-21 18:23:25 +0200

    v1.3
    Several fixes

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,4 @@
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("accordionOpdr");
     var i;
     for (i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
